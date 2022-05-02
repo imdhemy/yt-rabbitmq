1cedf147e60a87526db8d922d4b45e8f6da7ae53
Mohamad Eldhemy <imdhemy@gmail.com>
2022-05-02 17:49:49 +0300

    update the receive to simulate the worker

--- public/worker.php
+++ public/worker.php
@@ -14,6 +14,10 @@ echo " [*] Waiting for messages. To exit press CTRL+C\n";
 // Consume a queue
 $callback = function ($msg) {
     echo ' [x] Received ', $msg->body, "\n";
+
+    sleep(substr_count($msg->body, '.'));
+    
+    echo " [x] Done\n";
 };
 
 $channel->basic_consume('hello', '', false, true, false, false, $callback);
