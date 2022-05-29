223a92bf181d6c17a8453ca0d8c54fdc55b8a9bc
Mohamad Eldhemy <imdhemy@gmail.com>
2022-05-29 11:47:50 +0300

    add a worker that fails

---
+++ public/failure.php
@@ -0,0 +1,31 @@
+<?php
+
+use PhpAmqpLib\Channel\AbstractChannel;
+use PhpAmqpLib\Connection\AbstractConnection;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+require_once __DIR__ . '/../bootstrap.php';
+
+/** @var AMQPChannel|AbstractChannel $channel */
+/** @var AMQPStreamConnection|AbstractConnection $connection */
+
+echo " [*] Waiting for messages. To exit press CTRL+C\n";
+
+// Consume a queue
+$callback = function (AMQPMessage $msg) {
+    echo ' [x] Received ', $msg->body, "\n";
+
+    sleep(substr_count($msg->body, '.'));
+
+    echo " [x] Done\n";
+
+    throw new Exception('I failed :\'(');
+};
+
+$channel->basic_consume('hello', '', false, false, false, false, $callback);
+
+// Block while channel has callbacks
+while ($channel->is_open()) {
+    $channel->wait();
+}
