33a98bc10aa9e9e193a769bc240ae83d3ad0801e
Mohamad Eldhemy <imdhemy@gmail.com>
2022-05-29 11:39:57 +0300

    enable message acknowledgment

--- public/worker.php
+++ public/worker.php
@@ -3,6 +3,7 @@
 use PhpAmqpLib\Channel\AbstractChannel;
 use PhpAmqpLib\Connection\AbstractConnection;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
 
 require_once __DIR__ . '/../bootstrap.php';
 
@@ -12,15 +13,17 @@ require_once __DIR__ . '/../bootstrap.php';
 echo " [*] Waiting for messages. To exit press CTRL+C\n";
 
 // Consume a queue
-$callback = function ($msg) {
+$callback = function (AMQPMessage $msg) {
     echo ' [x] Received ', $msg->body, "\n";
 
     sleep(substr_count($msg->body, '.'));
-    
+
     echo " [x] Done\n";
+
+    $msg->ack();
 };
 
-$channel->basic_consume('hello', '', false, true, false, false, $callback);
+$channel->basic_consume('hello', '', false, false, false, false, $callback);
 
 // Block while channel has callbacks
 while ($channel->is_open()) {
